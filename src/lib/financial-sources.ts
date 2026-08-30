1e178ae560d7a94adb4746090d48eec5bc6cc54c
Kei <22tititan@gmail.com>
2026-08-30 11:05:01 +0900

    Connect OSAT and substrate source registry

--- src/lib/financial-sources.ts
+++ src/lib/financial-sources.ts
@@ -3,6 +3,7 @@ import v02Sources from '../data/sources-v02.json';
 import documentSources from '../data/document-sources.json';
 import v04DocumentSources from '../data/document-sources-v04.json';
 import v04Batch10DocumentSources from '../data/document-sources-v04-batch10.json';
+import v04Batch11DocumentSources from '../data/document-sources-v04-batch11.json';
 
 export const financialSources = [
   ...sources,
@@ -10,6 +11,7 @@ export const financialSources = [
   ...documentSources,
   ...v04DocumentSources,
   ...v04Batch10DocumentSources,
+  ...v04Batch11DocumentSources,
 ];
 
 export const financialSourceById = new Map(financialSources.map(source => [source.id, source]));
