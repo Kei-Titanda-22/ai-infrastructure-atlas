35c5da371d06085b3369065ac5beb71ce51b1b25
Kei <22tititan@gmail.com>
2026-08-30 12:54:37 +0900

    Connect power semiconductor source registry

--- src/lib/financial-sources.ts
+++ src/lib/financial-sources.ts
@@ -11,6 +11,7 @@ import v04Batch15DocumentSources from '../data/document-sources-v04-batch15.json
 import v04Batch16DocumentSources from '../data/document-sources-v04-batch16.json';
 import v04Batch17DocumentSources from '../data/document-sources-v04-batch17.json';
 import v04Batch18DocumentSources from '../data/document-sources-v04-batch18.json';
+import v04Batch19DocumentSources from '../data/document-sources-v04-batch19.json';
 
 export const financialSources = [
   ...sources,
@@ -26,6 +27,7 @@ export const financialSources = [
   ...v04Batch16DocumentSources,
   ...v04Batch17DocumentSources,
   ...v04Batch18DocumentSources,
+  ...v04Batch19DocumentSources,
 ];
 
 export const financialSourceById = new Map(financialSources.map(source => [source.id, source]));
